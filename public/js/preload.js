883199f906f2955b03e9ee6e0fed4fedcfe0c58b
zishe <alderkireyev@gmail.com>
2012-09-07 05:15:45 +0400

    heroku deploy

--- public/js/preload.js
+++ public/js/preload.js
@@ -1,5 +1,3 @@
-if (window.location.hash == '#_=_') {
-    window.location.hash = ''; // for older browsers, leaves a # behind
-    // history.pushState('', document.title, window.location.pathname); // nice and clean
-    e.preventDefault(); // no page reload
+if (window.location.hash !== '') {
+    window.location.hash = '';
 }
